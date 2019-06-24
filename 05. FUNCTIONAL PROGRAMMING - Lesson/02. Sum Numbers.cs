1e952c9102d0c70b1f600a8531773b8880192f66
BoryanaLen <44115110+BoryanaLen@users.noreply.github.com>
2019-06-24 17:24:40 +0300

    Create 02. Sum Numbers.cs

---
+++ 05. FUNCTIONAL PROGRAMMING - Lesson/02. Sum Numbers.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToList();
+
+            Console.WriteLine($"{numbers.Count}");
+
+            Console.WriteLine($"{numbers.Sum()}");
+        }
+    }
+}
