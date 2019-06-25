46fc92cd757e47c004f71ecbe57a7875284ff625
BoryanaLen <44115110+BoryanaLen@users.noreply.github.com>
2019-06-25 08:37:59 +0300

    Create 02. Knights of Honor.cs

---
+++ 05. FUNCTIONAL PROGRAMMING - Exercises/02. Knights of Honor.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Sir {name}");
+            }
+        }
+    }
+}
