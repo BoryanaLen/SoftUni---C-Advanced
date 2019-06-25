64598a4295a724b9aaebb28cb80fae8afbaa4d51
BoryanaLen <44115110+BoryanaLen@users.noreply.github.com>
2019-06-25 08:38:49 +0300

    Create 03. Custom Min Function.cs

---
+++ 05. FUNCTIONAL PROGRAMMING - Exercises/03. Custom Min Function.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToList();
+
+            numbers.Sort();
+
+            Console.WriteLine($"{numbers.FirstOrDefault()}");
+        }
+    }
+}
