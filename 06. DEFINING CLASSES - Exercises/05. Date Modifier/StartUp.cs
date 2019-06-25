7e0602fb97c6ef021803379bc78edb4e5b6f9d16
BoryanaLen <44115110+BoryanaLen@users.noreply.github.com>
2019-06-25 09:46:50 +0300

    Add files via upload

---
+++ 06. DEFINING CLASSES - Exercises/05. Date Modifier/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DateModifier dateModifier = new DateModifier();
+
+            string firstDate = Console.ReadLine();
+
+            string secondDate = Console.ReadLine();
+
+            Console.WriteLine(dateModifier.DifferenceBetweenDates(firstDate,secondDate));
+        }
+    }
+}
