beb295d19c94050151cc8f48a327c835a4bf0755
BoryanaLen <44115110+BoryanaLen@users.noreply.github.com>
2019-06-25 09:41:58 +0300

    Add files via upload

---
+++ 06. DEFINING CLASSES - Exercises/03. Oldest Family Member/Family.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> Members;
+
+        public Family()
+        {
+            this.Members = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.Members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.Members.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+    }
+}
---
+++ 06. DEFINING CLASSES - Exercises/03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberLines = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < numberLines; i++)
+            {
+                List<string> inputInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                string name = inputInfo[0];
+                int age = int.Parse(inputInfo[1]);
+
+                Person newMember = new Person(name, age);
+
+                if (newMember != null)
+                {
+                    family.AddMember(newMember);
+                }
+            }
+
+            Person oldestMember = family.GetOldestMember();
+
+            if (oldestMember != null)
+            {
+                Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+            }
+        }
+    }
+}
