94ea8bf0509c56ffd7deec62fff174245a0b5502
Boryana Lengerova <b.arizanova@abv.bg>
2019-06-25 12:49:14 +0300

    Add files

---
+++ 08. GENERICS - Lesson/01. Box/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> boxList;
+
+        public int Count
+        {
+            get { return this.boxList.Count; }
+        }
+
+        public Box()
+        {
+            boxList = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            boxList.Add(element);
+        }
+
+        public T Remove()
+        {
+            var item =  boxList[Count-1];
+            this.boxList.RemoveAt(Count - 1);
+            return item;
+        }
+    }
+}
---
+++ 08. GENERICS - Lesson/02. Array Creator/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create(5, "Ivan");
+
+            int[] integers = ArrayCreator.Create(10, 1);
+        }
+    }
+}
